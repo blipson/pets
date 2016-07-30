6dce3dc2b6eb3abf2bc1c90840b80eecbc015986
Ben Lipson <Ben@Bens-MacBook-Pro.local>
2016-07-30 17:15:27 -0500

    Update name in server.js

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var app = express();
 app.use(express.static('petsApp'));
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Pets app listening on port 3000');
 });
