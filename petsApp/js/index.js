2c1ac44112d04f8b3c21ed59d2ea7fc479a6983d
Ben Lipson <Ben@Bens-MacBook-Pro.local>
2016-08-01 00:19:10 -0500

    Make it a single page for the search results, will still need other routes later

--- petsApp/js/index.js
+++ petsApp/js/index.js
@@ -15,6 +15,19 @@
         'background-size': 'cover'
     };
     $scope.zip = "";
+    $scope.petKey = "688edd00993ee8e4c054a8e2d77b8271";
+    $scope.pets = [];
+    $scope.showLoadingIcon = false;
+
+    $scope.getPets = function(zip) {
+      $scope.showLoadingIcon = true;
+      $http.jsonp("http://api.petfinder.com/pet.find?format=json&key=" + $scope.petKey + "&location=" + $scope.zip + "&callback=JSON_CALLBACK")
+      .then(function(response) {
+        console.log(response);
+        $scope.showLoadingIcon = false;
+        $scope.pets = response.data.petfinder.pets.pet;
+      });
+    }
 
     $scope.go = function ( path ) {
       $window.location.href = path + $scope.zip;
